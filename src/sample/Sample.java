4517833cc37dbe8453db9682b86854d845252df1
elia226 <pitogoeliza079@gmail.com>
2025-08-17 11:23:01 +0800

    updating

--- src/sample/Sample.java
+++ src/sample/Sample.java
@@ -15,7 +15,7 @@ public class Sample {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        System.out.println("hello kalibutan nakooooooooo");
+        System.out.println("hello kalibutan honeybunch nakoooooooooooooooooooo");
     }
     
 }
